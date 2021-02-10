edf1b954a202c58d5cc73a5b69276454e571f460
caiquebslima <caiquebslima@gmail.com>
2021-02-10 20:21:43 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/header.scss';
+
+function Header() {
+  return (
+    <header>
+      <h2>Omni</h2>
+      <aside>
+        <button>Sair</button>
+      </aside>
+    </header>
+  );
+}
+
+export default Header;
