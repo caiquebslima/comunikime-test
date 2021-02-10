48af068420e03e6845f9f9c446c35d1ab873b3d4
caiquebslima <caiquebslima@gmail.com>
2021-02-10 20:40:39 -0300

    feat: created data list to populate sidenav

---
+++ src/components/NavData.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import ChatIcon from '@material-ui/icons/Chat';
+import ShowChartIcon from '@material-ui/icons/ShowChart';
+import TimelapseIcon from '@material-ui/icons/Timelapse';
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+import PersonIcon from '@material-ui/icons/Person';
+import HistoryIcon from '@material-ui/icons/History';
+import SettingsIcon from '@material-ui/icons/Settings';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
+import LayersIcon from '@material-ui/icons/Layers';
+import EditIcon from '@material-ui/icons/Edit';
+import MoodBadIcon from '@material-ui/icons/MoodBad';
+import FolderSpecialIcon from '@material-ui/icons/FolderSpecial';
+
+export const NavData = [
+  {
+    title: 'Dashboards',
+    icon: <DashboardIcon />,
+    iconClose: <ArrowDropUpIcon />,
+    iconOpen: <ArrowDropDownIcon />,
+    subNav: [
+      {
+        title: 'Chat',
+        icon: <ChatIcon />,
+      },
+      {
+        title: 'Gráfico por Mídia',
+        icon: <ShowChartIcon />,
+      },
+      {
+        title: 'Tempo Real',
+        icon: <TimelapseIcon />,
+      },
+    ],
+  },
+  {
+    title: 'WhatsApp RPA',
+    icon: <WhatsAppIcon />,
+  },
+  {
+    title: 'Jornada do Cliente',
+    icon: <PersonIcon />,
+  },
+  {
+    title: 'Histórico Geral',
+    icon: <HistoryIcon />,
+  },
+  {
+    title: 'Configurações',
+    icon: <SettingsIcon />,
+    iconClose: <ArrowDropUpIcon />,
+    iconOpen: <ArrowDropDownIcon />,
+    subNav: [
+      {
+        title: 'Grupos de Disposições',
+        icon: <LayersIcon />,
+      },
+      {
+        title: 'Editar Mídias',
+        icon: <EditIcon />,
+      },
+      {
+        title: 'Controle Bad Words',
+        icon: <MoodBadIcon />,
+      },
+      {
+        title: 'Operação OMN!A',
+        icon: <FolderSpecialIcon />,
+      },
+    ],
+  },
+];
