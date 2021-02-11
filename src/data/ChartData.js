11f146f52ff8c06eee3ab89b1ff49e835ec45471
caiquebslima <caiquebslima@gmail.com>
2021-02-11 14:00:26 -0300

    Reorganized data into own folder Added Chart Icon and Component

---
+++ src/data/ChartData.js
@@ -0,0 +1,20 @@
+import ChartIcon from '../assets/ChartIcon';
+
+export const ChartData = [
+  {
+    icon: <ChartIcon />,
+    type: 'Humanos',
+    counter: 789,
+    background: '#4caf50',
+  },
+  {
+    icon: <ChartIcon />,
+    type: 'Bots',
+    counter: 2003,
+  },
+  {
+    icon: <ChartIcon />,
+    type: 'Contatos por dia',
+    counter: 2792,
+  },
+];
