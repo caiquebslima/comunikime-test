cc5bedde658fb3be29b203f6a9137da0da59090f
caiquebslima <caiquebslima@gmail.com>
2021-02-11 11:17:15 -0300

    feat: added all counters and styles

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,7 +3,11 @@ import '../styles/counter.scss';
 function Counter({ item }) {
   return (
     <div className='counter'>
-      <figure className={item.color}>{item.icon}</figure>
+      <figure
+        style={{ background: `${item.background}`, color: `${item.color}` }}
+      >
+        {item.icon}
+      </figure>
       <div className='counter__info'>
         <p className='number'>{item.counter}</p>{' '}
         <p className='legend'>Contatos</p>{' '}
--- src/data/CounterData.jsx
+++ src/data/CounterData.jsx
@@ -3,26 +3,51 @@ import IconEmail from '../assets/IconEmail';
 import IconMessenger from '../assets/IconMessenger';
 import IconWhatsApp from '../assets/IconWhatsApp';
 import IconWeChat from '../assets/IconWeChat';
+import IconTwitter from '../assets/IconTwitter';
+import IconFacebook from '../assets/IconFacebook';
+import IconTelegram from '../assets/IconTelegram';
 
 export const CounterData = [
   {
     icon: <IconEmail />,
-    color: 'email',
-    counter: 0,
+    color: '#fff',
+    background: '#007fd0',
+    counter: 333,
+  },
+  {
+    icon: <IconTelegram />,
+    color: '#fff',
+    background: '#039be5',
+    counter: 210,
   },
   {
     icon: <IconMessenger />,
-    color: 'messenger',
-    counter: 0,
+    color: '#fff',
+    background: '#2196f3',
+    counter: 190,
   },
   {
     icon: <IconWhatsApp />,
-    color: 'whatsapp',
-    counter: 0,
+    color: '#fff',
+    background: '#64b161',
+    counter: 500,
   },
   {
     icon: <IconWeChat />,
-    color: 'wechat',
-    counter: 0,
+    color: '#fff',
+    background: '#4caf50',
+    counter: 30,
+  },
+  {
+    icon: <IconTwitter />,
+    color: '#fff',
+    background: '#1bb1e4',
+    counter: 50,
+  },
+  {
+    icon: <IconFacebook />,
+    color: '#fff',
+    background: '#4064ac',
+    counter: 200,
   },
 ];
